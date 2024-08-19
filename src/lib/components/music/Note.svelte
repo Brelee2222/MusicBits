57e299ef8addd05177afa7a566393b9085e79dfb
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2024-08-19 14:34:58 -0700

    Made minmNote

--- src/lib/components/music/Note.svelte
+++ src/lib/components/music/Note.svelte
@@ -5,7 +5,7 @@
 </script>
 
 {#if noteBeat.showStem}
-    <line x1=".05" y1=".568" x2=".05" y2="5" stroke="black" stroke-width="0.1"/>
+    <!-- <line x1=".05" y1=".568" x2=".05" y2="5" stroke="black" stroke-width="0.1"/> -->
 {/if}
 
-<image href="/testNote.svg" height="1"/>
+<image href="/notes/minmNote.svg" height="1"/>
